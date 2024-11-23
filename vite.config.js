66d3160c8f9efc200c00f2c0fd1abce4283ba795
Ricardo <theartisanmidia@gmail.com>
2024-11-23 20:20:16 -0300

    Cambio en vite para docs

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/trading-app/' : '/', // Ajusta automáticamente el `base`
+  base: process.env.NODE_ENV === 'production' ? '/trading-frontend/' : '/', // Ajusta automáticamente el `base`
   build: {
     outDir: 'docs', // Cambiar 'dist' por 'docs'
   },
