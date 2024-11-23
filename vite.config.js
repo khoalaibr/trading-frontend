8b2b92a6e78fabc1a5fd766a1dff7eff4f757b2b
Ricardo <theartisanmidia@gmail.com>
2024-11-23 20:16:17 -0300

    Cambio relativaas

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/trading-app/' : '/', // Ajusta automáticamente el `base`
+  build: {
+    outDir: 'docs', // Cambiar 'dist' por 'docs'
+  },
 });
 
