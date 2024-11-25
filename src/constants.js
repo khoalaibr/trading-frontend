6f80c4597eb53186596db593a0c94a2fe4bfb9f7
Ricardo <theartisanmidia@gmail.com>
2024-11-25 08:12:20 -0300

    80 acciones

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,85 @@
 export const B3_ACTIONS = [
-    'PETR4.SA', 
-    'VALE3.SA', 
-    'ITUB4.SA'
-]; // Puedes agregar más acciones
+    'PETR4.SA', // Petrobras
+    'VALE3.SA', // Vale
+    'ITUB4.SA', // Itaú Unibanco
+    'BBDC4.SA', // Bradesco
+    'ABEV3.SA', // Ambev
+    'BBAS3.SA', // Banco do Brasil
+    'B3SA3.SA', // B3
+    'WEGE3.SA', // Weg
+    'RENT3.SA', // Localiza
+    'LREN3.SA', // Lojas Renner
+    'MGLU3.SA', // Magazine Luiza
+    'VIVT3.SA', // Telefônica Brasil
+    'GGBR4.SA', // Gerdau
+    'CSNA3.SA', // CSN
+    'SUZB3.SA', // Suzano
+    'JBSS3.SA', // JBS
+    'RAIL3.SA', // Rumo
+    'KLBN11.SA', // Klabin
+    'EGIE3.SA', // Engie Brasil
+    'EQTL3.SA', // Equatorial
+    'TIMS3.SA', // TIM Brasil
+    'CPLE6.SA', // Copel
+    'PRIO3.SA', // PetroRio
+    'CVCB3.SA', // CVC Brasil
+    'TOTS3.SA', // Totvs
+    'HAPV3.SA', // Hapvida
+    'CYRE3.SA', // Cyrela
+    'BRKM5.SA', // Braskem
+    'USIM5.SA', // Usiminas
+    'FLRY3.SA', // Fleury
+    'ENBR3.SA', // Energias do Brasil
+    'MRFG3.SA', // Marfrig
+    'PSSA3.SA', // Porto Seguro
+    'NTCO3.SA', // Natura
+    'HYPE3.SA', // Hypera
+    'AMER3.SA', // Americanas
+    'BEEF3.SA', // Minerva
+    'BRFS3.SA', // BRF
+    'CPFE3.SA' // CPFL Energia
+];
+
 export const AMERICAN_ACTIONS = [
-    'AAPL', 
-    'GOOGL', 
-    'AMZN'
-]; // Puedes agregar más acciones
+    'AAPL', // Apple Inc.
+    'GOOGL', // Alphabet Inc.
+    'AMZN', // Amazon.com Inc.
+    'MSFT', // Microsoft Corp.
+    'META', // Meta Platforms Inc.
+    'TSLA', // Tesla Inc.
+    'NVDA', // NVIDIA Corp.
+    'AMD', // Advanced Micro Devices Inc.
+    'NFLX', // Netflix Inc.
+    'INTC', // Intel Corp.
+    'CRM', // Salesforce Inc.
+    'PYPL', // PayPal Holdings Inc.
+    'ORCL', // Oracle Corp.
+    'ADBE', // Adobe Inc.
+    'CSCO', // Cisco Systems Inc.
+    'QCOM', // Qualcomm Inc.
+    'SHOP', // Shopify Inc.
+    'UBER', // Uber Technologies Inc.
+    'SQ', // Block Inc. (Square)
+    'ROKU', // Roku Inc.
+    'SNOW', // Snowflake Inc.
+    'PLTR', // Palantir Technologies Inc.
+    'TWLO', // Twilio Inc.
+    'SPOT', // Spotify Technology SA
+    'SE', // Sea Ltd.
+    'BABA', // Alibaba Group Holding Ltd.
+    'JD', // JD.com Inc.
+    'PDD', // Pinduoduo Inc.
+    'ZM', // Zoom Video Communications Inc.
+    'DIS', // The Walt Disney Co.
+    'NKE', // Nike Inc.
+    'BA', // Boeing Co.
+    'T', // AT&T Inc.
+    'V', // Visa Inc.
+    'MA', // Mastercard Inc.
+    'KO', // Coca-Cola Co.
+    'PEP', // PepsiCo Inc.
+    'MCD', // McDonald's Corp.
+    'WMT', // Walmart Inc.
+    'CVX' // Chevron Corp.
+];
+
